02c1c5c37a34f80ff20b5557690526375941a15f
Jared Steiner <steiner.jared1996@gmail.com>
2021-06-26 17:09:58 +1000

    Add CLI With Options to override Env Vars and Appsettings config. #4

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,37 @@
 use anyhow;
+use clap::Clap;
 use log;
 use simplelog::{Config, LevelFilter, TermLogger, TerminalMode};
 
+mod cli;
 mod settings;
 use settings::Settings;
 
+use crate::cli::Opts;
+
 fn main() -> anyhow::Result<()> {
+    let opts = Opts::parse();
+    let setings = Settings::new(opts)?;
+
     TermLogger::init(
-        LevelFilter::Debug,
+        log_level(&setings),
         Config::default(),
         TerminalMode::Mixed,
         simplelog::ColorChoice::Always,
     )?;
     log::info!("Starting up...");
-    Settings::new()?;
-    log::info!("Setup Config");
+
+    log::info!("Setup Config {:#?}", setings);
+
     Ok(())
+}
+
+fn log_level(settings: &Settings) -> LevelFilter {
+    if settings.debug {
+        LevelFilter::Debug
+    } else if settings.quiet {
+        LevelFilter::Error
+    } else {
+        LevelFilter::Info
+    }
 }
--- src/settings.rs
+++ src/settings.rs
@@ -1,20 +1,65 @@
+use crate::cli::Opts;
 use config::{Config, ConfigError, Environment, File};
 use serde::Deserialize;
 use std::env;
 
 #[derive(Debug, Deserialize)]
+pub struct Peers {
+    /// later a list of urls or ips that the application can use to discover peers
+    pub source: String,
+}
+
+#[derive(Debug, Deserialize)]
+pub struct Data {
+    /// Local path for sled data store
+    pub path: String,
+}
+
+#[derive(Debug, Deserialize)]
 pub struct Settings {
-    pub data_path: String,          // Local path for sled data store
-    pub peer_discovery_src: String, // later a list of urls or ips that the application can use to discover peers
+    pub debug: bool,
+    pub quiet: bool,
+    pub data: Data,
+    pub peers: Peers,
 }
 
 impl Settings {
-    pub fn new() -> Result<Self, ConfigError> {
+    pub fn new(opts: Opts) -> Result<Self, ConfigError> {
         let mut s = Config::new();
-        s.merge(File::with_name("config.yaml"))?;
+
+        // Config File
+        s.merge(File::with_name("appsettings"))?;
+
+        // Development Environment Overrides
         let env = env::var("RUN_MODE").unwrap_or_else(|_| "development".into());
-        s.merge(File::with_name(&format!("config/{}", env)).required(false))?;
-        s.merge(Environment::with_prefix("app"))?;
+        s.merge(File::with_name(&format!("config/appsettings.{}", env)).required(false))?;
+
+        // Environment Variables
+        s.merge(Environment::with_prefix("app").separator("_"))?;
+
+        // CLI Opts
+        include_cli_opts(&mut s, opts)?;
+
         s.try_into()
     }
+}
+
+fn include_cli_opts(s: &mut Config, opts: Opts) -> Result<(), ConfigError> {
+    if opts.debug {
+        s.set("debug", true)?;
+    }
+
+    if opts.quiet {
+        s.set("quiet", true)?;
+    }
+
+    if let Some(d) = opts.data_path {
+        s.set("data.path", d)?;
+    }
+
+    if let Some(p) = opts.peer_src {
+        s.set("peers.source", p)?;
+    }
+
+    Ok(())
 }
