a522b631ba9b31a97bf4f735b00896b3cf5b9bef
Tom Watson <tom_w@fastmail.com>
2021-06-27 16:48:38 +1000

    Add some rudimentary logic to bootstrap the mesh from provided configs

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,16 @@
+use blip::Mesh;
 use clap::Clap;
+use log::info;
 use simplelog::{Config, LevelFilter, TermLogger, TerminalMode};
+use std::net::SocketAddr;
 
 mod cli;
 mod settings;
+use cli::Opts;
 use settings::Settings;
 
-use crate::cli::Opts;
-
-fn main() -> anyhow::Result<()> {
+#[tokio::main]
+async fn main() -> anyhow::Result<()> {
     let opts = Opts::parse();
     TermLogger::init(
         log_level(&opts),
@@ -15,11 +18,27 @@ fn main() -> anyhow::Result<()> {
         TerminalMode::Mixed,
         simplelog::ColorChoice::Always,
     )?;
-    log::info!("Starting up...");
-
-    let setings = Settings::new(opts)?;
-    log::info!("Setup Config {:#?}", setings);
-
+    let config = Settings::new(opts)?;
+    log::info!("Setup Config {:#?}", config);
+    info!("Setup Config. Starting mesh");
+    let mesh = Mesh::low_latency();
+    info!("Low-latency-mesh setup");
+    let listen_addr = match config.serve_address {
+        Some(addr) => addr,
+        None => {
+            let default_serve_adr: SocketAddr = "[::1]:8901".parse()?;
+            default_serve_adr
+        }
+    };
+    log::info!("Listening on port: {}", listen_addr.port());
+    if let Some(peer_sock_addr) = config.peers.address {
+        mesh.join_seed(peer_sock_addr, false)
+            .serve(listen_addr)
+            .await?;
+    } else {
+        log::info!("No peer node provided to bootstrap from: starting as single node");
+        mesh.serve(listen_addr).await?;
+    };
     Ok(())
 }
 
