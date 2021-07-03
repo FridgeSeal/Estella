706f3d25f8493d64f695e02eab5dd2c35e04235b
Tom Watson <tom_w@fastmail.com>
2021-07-04 00:48:32 +1000

    Start splitting membership/networking functionality into separate module

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,13 @@
-use blip::Mesh;
 use clap::Clap;
+use evmap;
 use log::info;
 use rand::Rng;
 use simplelog::{Config, LevelFilter, TermLogger, TerminalMode};
-use std::net::SocketAddr;
-
+use tonic;
 mod cli;
+mod membership_mesh;
 mod settings;
+
 use cli::Opts;
 use settings::Settings;
 
@@ -22,23 +23,7 @@ async fn main() -> anyhow::Result<()> {
     let config = Settings::new(opts)?;
     log::info!("Setup Config {:#?}", config);
     info!("Setup Config. Starting mesh");
-    let mesh = Mesh::low_latency();
-    info!("Low-latency-mesh setup");
-    let listen_addr = {
-        let mut rng = rand::thread_rng();
-        let mut default_serve_adr: SocketAddr = "[::1]:1234".parse()?;
-        default_serve_adr.set_port(rng.gen_range(1024..65000));
-        default_serve_adr
-    };
-    log::info!("Listening on port: {}", listen_addr.port());
-    if let Some(peer_sock_addr) = config.peers.address {
-        mesh.join_seed(peer_sock_addr, false)
-            .serve(listen_addr)
-            .await?;
-    } else {
-        log::info!("No peer node provided to bootstrap from: starting as single node");
-        mesh.serve(listen_addr).await?;
-    };
+    let (handle, hmap) = membership_mesh::boostrap(&config).await?;
     Ok(())
 }
 
